f4ff78b735a9093b7905aab1ff0f1692de6691f9
henryyang42 <henryyang42@gmail.com>
2014-11-27 18:29:03 +0800

    First commit

---
+++ clarification/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+# Create your views here.
+
+def home(request):
+    excuses = [
+        "It was working in my head",
+        "I thought I fixed that",
+        "Actually, that is a feature",
+        "It works on my machine",
+    ]
+
+    excuse = random.choice(excuses)
+    return render(request, "base.html", {'excuse': excuse})

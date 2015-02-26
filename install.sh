df22d993db2649a3eb118177bdd13358e3036e59
henryyang42 <henryyang42@gmail.com>
2015-02-26 15:14:25 +0800

    add auto install

--- install.sh
+++ install.sh
@@ -1,8 +1,21 @@
+# install some needed python package
 pip install beautifulsoup4
 pip install django-grappelli
 pip install django-pagedown
 pip install django-markdown-deux
 pip install django-crispy-forms
+
+# gmail password
+GMAIL_PWD=ISeaTeLContestSite/password.py
+
+if [ -f $GMAIL_PWD ];
+then
+   echo "File $GMAIL_PWD exists."
+else
+   echo "File $GMAIL_PWD does not exist."
+   read -p "Enter contest site gmail password: " PWD
+   echo "GMAIL_PASSWORD = '$PWD'" >> $GMAIL_PWD
+fi
 
 python manage.py collectstatic --noinput
 alias dj='python manage.py'
